bf95ff2aad8212f46af4ddbaabafb42e573f5f91
Miphreal Adler <miph@miph-nb.(none)>
2012-12-13 02:38:59 +0300

    Added a couple test graphics

--- ui/frames/main.py
+++ ui/frames/main.py
@@ -5,7 +5,11 @@ Main Window. It contains basic UI.
 
 
 import os
+
 import wx
+import numpy as np
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg as FigCanvas
 
 from lib.i18n import gettext as _
 
@@ -14,7 +18,7 @@ class MainWindow(wx.Frame):
     title = _('Digital Signal Processing')
 
     def __init__(self):
-        wx.Frame.__init__(self, None, -1, self.title)
+        wx.Frame.__init__(self, None, -1, self.title, size=(800, 500))
 
         self.create_menu()
         self.create_tool_bar()
@@ -40,6 +44,10 @@ class MainWindow(wx.Frame):
         m_info = menu_signal.Append(-1, _('Signal Info'))
         self.menu_bar.Append(menu_signal, _('Signal'))
 
+        menu_about = wx.Menu()
+        m_author = menu_about.Append(-1, _('Author'))
+        self.menu_bar.Append(menu_about, _('About'))
+
         # bind events
         self.Bind(wx.EVT_MENU, self.on_export_image, m_expt)
         self.Bind(wx.EVT_MENU, self.on_exit, m_exit)
@@ -54,7 +62,42 @@ class MainWindow(wx.Frame):
         self.status_bar = self.CreateStatusBar()
 
     def create_main_panel(self):
-        pass
+        #test behavior
+        self.panel = wx.Panel(self)
+        self.dpi = 100
+        self.fig = Figure(dpi=self.dpi, facecolor='w')
+
+
+        def f(t):
+            return np.exp(-t) * np.cos(2*np.pi*t)
+
+        t1 = np.arange(0.0, 5.0, 0.1)
+        t2 = np.arange(0.0, 5.0, 0.02)
+
+        plt = self.fig.add_subplot(311)
+        plt.plot(t1, f(t1), 'bo', t2, f(t2), 'k')
+
+        plt = self.fig.add_subplot(312)
+        plt.plot(t2, np.cos(2*np.pi*t2), 'r--')
+
+        mu, sigma = 100, 15
+        x = mu + sigma * np.random.randn(10000)
+
+        plt = self.fig.add_subplot(313)
+        # the histogram of the data
+        n, bins, patches = plt.hist(x, 50, normed=1, facecolor='g', alpha=0.75)
+
+        plt.xaxis.set_label('Smarts')
+        plt.yaxis.set_label('Probability')
+        plt.set_title('Histogram of IQ')
+        plt.text(60, .025, r'$\mu=100,\ \sigma=15$')
+
+        plt.axis([40, 160, 0, 0.03])
+        plt.grid(True)
+
+
+        self.canvas = FigCanvas(self.panel, -1, self.fig)
+        self.canvas.draw()
 
     ##
     # Event handling
